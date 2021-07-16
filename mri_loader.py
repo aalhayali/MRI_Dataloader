31f7e34a37bf798999ba502ca52cafca5dbf86f2
sathvikbhagavan <35105271+sathvikbhagavan@users.noreply.github.com>
2021-07-16 22:10:07 +0530

    Update mri_loader.py

--- mri_loader.py
+++ mri_loader.py
@@ -1,33 +1,44 @@
-#Import libraries
+# Import libraries
 
 import nibabel as nib
 import numpy as np
 import os
 from tqdm.notebook import tqdm
 
-#Dataloader Class
+# Dataloader Class
 
 class DataLoaderMRILoc:
 
     def __init__(self, directory, height, width, channels, channels_first):
+        
+        # Directory of all the images
         self.directory = directory
+        
+        # Image Attributes
         self.height = height
         self.width = width
         self.channels = channels
+        
+        # Format of the image (Bool)
         self.channels_first = channels_first
 
     def get_data(self):
-        files = os.listdir(self.directory) #list files in directory
+        
+        # List files in directory
+        files = os.listdir(self.directory)
 
-        #What's the purpose of this line?
+        # Desired shape of the volume along with the opposite
+        # This would check for both channel first and channel last images and convert them to the specified format
         if self.channels_first:
             desired_shape = (self.channels, self.height, self.width)
             desired_shape_opposite = (self.height, self.width, self.channels)
         else:
             desired_shape = (self.height, self.width, self.channels)
             desired_shape_opposite = (self.channels, self.height, self.width)
-
+       
         X = []
+        
+        # Names of files whose shape is other than the desired shape (except channel positions)
         incorrect_shaped_files = []
 
         for i, file in enumerate(tqdm(files)):
@@ -48,7 +59,18 @@ class DataLoaderMRILoc:
 
         return np.array(X), incorrect_shaped_files
 
-data_loader = DataLoaderMRILoc('images', 512, 512, 7, False)
+
+    
+# Example
+directory = 'images'
+height = 512
+width = 512
+channels = 7
+channels_first = False
+
+data_loader = DataLoaderMRILoc(directory, height, width, channels, channels_first)
 X, incorrect_shaped_files = data_loader.get_data()
-X.shape
-incorrect_shaped_files
+
+# Checking
+print(X.shape)
+print(incorrect_shaped_files)
